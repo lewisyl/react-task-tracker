98cc79f8b0e1c6e0f753b52ea403e88de5011325
Lewis Liu <lewisliuusa@outlook.com>
2021-02-28 16:23:25 -0800

    a

--- src/App.js
+++ src/App.js
@@ -26,13 +26,17 @@ function App() {
 
 	// Delete Task
 	const deleteTask = (id) => {
-		console.log("delete", id);
+		setTasks(tasks.filter((task) => task.id !== id));
 	};
 
 	return (
 		<div className="container">
 			<Header title="Task Tracker" />
-			<Tasks tasks={tasks} onDelete={deleteTask} />
+			{tasks.length > 0 ? (
+				<Tasks tasks={tasks} onDelete={deleteTask} />
+			) : (
+				"No Task Available"
+			)}
 		</div>
 	);
 }
