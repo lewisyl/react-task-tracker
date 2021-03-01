8cffb318726e619b741530f3f62612829c9fa233
Lewis Liu <lewisliuusa@outlook.com>
2021-02-28 20:16:41 -0800

    submit added

--- src/App.js
+++ src/App.js
@@ -25,6 +25,11 @@ function App() {
 		},
 	]);
 
+	// Add Task
+	const addTask = (task) => {
+		console.log(task);
+	};
+
 	// Delete Task
 	const deleteTask = (id) => {
 		setTasks(tasks.filter((task) => task.id !== id));
@@ -42,7 +47,7 @@ function App() {
 	return (
 		<div className="container">
 			<Header title="Task Tracker" />
-			<AddTask />
+			<AddTask onAdd={addTask} />
 			{tasks.length > 0 ? (
 				<Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
 			) : (
